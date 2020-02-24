b8a1cdc09fd03da09b6597ab5f20be4facea021f
Vishesh Javangula <vishesh.javangula@ibm.com@Visheshs-MacBook-Pro.local>
2020-02-24 17:59:13 -0500

    Added filtering and clearing

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ loadEventListeners();
 function loadEventListeners() {
   //Add task event
   form.addEventListener('submit', addTask);
+
+  //Remove Task event
+  taskList.addEventListener('click', removeTask);
+
+  //Clear tasks
+  clearBtn.addEventListener('click', clearTasks);
+
+  //Filter Tasks
+  filter.addEventListener('keyup', filterTasks);
 }
 
 
@@ -46,6 +55,43 @@ function addTask(e) {
     taskInput.value = '';
 
   }
+  e.preventDefault();
+
+}
+
+function removeTask(e){
+  if(e.target.parentElement.parentElement.className == 'collection-item'){
+    if(confirm('Are you sure homie?')){
+      e.target.parentElement.parentElement.remove();
+    }
+    e.preventDefault();
+  }
+}
+
+function clearTasks(e){
+
+  if (e.target.classList.contains('clear-tasks')){
+    while(taskList.firstChild){
+      taskList.removeChild(taskList.firstChild);
+    }
+
+  }
+
+}
+
+function filterTasks(e){
+  const text = document.getElementById('filter').value.toLowerCase();
+  document.querySelectorAll('.collection-item')
+  .forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){ //-1 if there is no match
+      task.style.display = 'block';
+
+    }else{
+      task.style.display = 'none';
+    }
+
+  });
   e.preventDefault();
 
 }
